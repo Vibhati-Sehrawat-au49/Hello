c9fa71b8168c3a95e7c2c36c2f0ae16c111661f0
Vibhati <108918266+Vibhati-Sehrawat-au49@users.noreply.github.com>
2022-09-18 20:05:21 +0530

    W3D1.js

---
+++ W3D1.js
@@ -0,0 +1,8 @@
+//Write a program to check if a number is divisible by four or not?
+//(no need to use if else, just print true if the number is divisible by four, otherwise print false)
+
+let a = 4;
+if (a%4==0)
+  console.log('true');
+else 
+  console.log('false');

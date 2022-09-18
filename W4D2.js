a07d6adea803aea1d25daf5c897f6315ea5a85da
Vibhati <108918266+Vibhati-Sehrawat-au49@users.noreply.github.com>
2022-09-18 20:44:10 +0530

    W4D2.js

---
+++ W4D2.js
@@ -0,0 +1,12 @@
+// Q1. What will be the output ? (Easy) (5 marks)
+
+function printMax(a, b){
+if (a > b){
+console.log(a, 'is maximum') }
+else if (a == b)
+console.log(a, 'is equal to', b)
+else
+console.log(b, 'is maximum') }
+printMax(3, 4)
+
+//Ans - 4 is maximum

bd563d805ffb11a3b59deb78a899fb5032bbfd71
Vibhati <108918266+Vibhati-Sehrawat-au49@users.noreply.github.com>
2022-09-18 20:41:23 +0530

    W4D1.js

---
+++ W4D1.js
@@ -0,0 +1,17 @@
+//Q-1) write a program to print “Hello World” 100 times, using for loop.
+
+for (let x=1;x<=100;x++)
+console.log("Hello World", x)
+
+
+//Q-2) write a program to find the biggest of 3 variables.
+let a = 78
+let b = 35
+let c = 66
+
+if (a>b && a>c )
+  console.log('a is biggest of the 3 numbers');
+else if (b>a && b>c) 
+  console.log('b is biggest of the 3 numbers');
+else if (c>a && c>b)
+  console.log('c is biggest of the 3 numbers');

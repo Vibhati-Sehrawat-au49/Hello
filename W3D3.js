3450f57d390f495c688209b2670675396f88c0a3
Vibhati <108918266+Vibhati-Sehrawat-au49@users.noreply.github.com>
2022-09-18 20:10:56 +0530

    W3D3.js

---
+++ W3D3.js
@@ -0,0 +1,15 @@
+// Q-1 ) Write a program to check if a number is even or odd? (7.5 marks)
+let d = 69
+if (d%2==0)
+  console.log('Number is Even')
+else 
+  console.log('Number is Odd')
+  
+// Q-2) Write a program to print truth-table of OR logical operator (7.5 marks)
+let a = 37
+let b = 39
+if (a%2==0 || b%2==0)
+  console.log('True')
+else
+  console.log('False')
+  

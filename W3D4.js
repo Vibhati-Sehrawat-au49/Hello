3564e879822f907ceb23563d647f6a542e8c7b00
Vibhati <108918266+Vibhati-Sehrawat-au49@users.noreply.github.com>
2022-09-18 20:13:21 +0530

    W3D4.js

---
+++ W3D4.js
@@ -0,0 +1,11 @@
+//Q1. What will be the output? (Easy) (15 marks)
+a=3
+b=4
+if (a > b)
+console.log(a, 'is maximum')
+else if (a == b)
+console.log(a, 'is equal to', b)
+else
+console.log(b, 'is maximum')
+
+//Ans - 4 is Maximum
